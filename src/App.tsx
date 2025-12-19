a59f46df4a700ec97b0d0a85e3c59adb481b9bf0
Mads Lee Giil <mads@giil.no>
2025-12-19 22:54:48 +0100

    Legg til ny post for å teste flere poster

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     return (
         <div className="block w-full">
             {posts.map((post) => (
-                <Link to={`/blog/${post.slug}`} key={post.slug} className="block bg-gray-200 w-full hover:bg-gray-300 transition-colors">
-                    <img src={post.imagePath} alt={"Eksempelbilde for posten"} />
+                <Link to={`/blog/${post.slug}`} key={post.slug} className="block bg-gray-200 w-full hover:bg-gray-300 transition-colors mb-8">
+                    {post.imagePath && <img src={post.imagePath} alt={"Eksempelbilde for posten"} />}
                     <div className="p-6">
                         <h3 className="text-2xl font-bold">{post.title}</h3>
                         <p className="text-gray-700">{formatDateReadable(post.date)}</p>
